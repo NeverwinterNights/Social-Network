dcaf75f9496e4eda8ec50464a7dfbd152f2e8396
Pavel Kardash <cardash@mail.ru>
2021-08-13 20:33:21 +0300

    Done 20

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import styles from "./Navbar.module.css"
-import {Dialogs} from "../Dialogs/Dialogs";
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
     <nav className={styles.nav}>
-        <div><a href="/profile">Profile</a></div>
-        <div><a href="/dialogs">Dialogs</a></div>
+        <div><NavLink activeClassName={styles.active} to="/profile">Profile</NavLink></div>
+        <div><NavLink activeClassName={styles.active} to="/dialogs">Dialogs</NavLink></div>
         <div><a>News</a></div>
         <div ><a>Music</a></div>
         <div><a>News</a></div>
