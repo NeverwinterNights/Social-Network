2954629262868d6cf4c11a08715433e2b938866c
Pavel Kardash <cardash@mail.ru>
2021-08-14 13:19:35 +0300

    Done 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import styles from "./Dialogs.module.css";
 
-export const Dialogs =() => {
- return (
-        <div>
-            Dialogs
+export const Dialogs = () => {
+    return (
+        <div className={styles.main}>
+            <div className={styles.dialogs}>
+                <div className={styles.dialog}>Ivan</div>
+                <div className={styles.dialog + " " + styles.active}>Oleg</div>
+                <div className={styles.dialog}>Misha</div>
+                <div className={styles.dialog}>Petr</div>
+                <div className={styles.dialog}>Alex</div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Privet</div>
+                <div className={styles.message}>How are you</div>
+                <div className={styles.message}>Lets we meet</div>
+                <div className={styles.message}>No</div>
+            </div>
         </div>
     );
 };
